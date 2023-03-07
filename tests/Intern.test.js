a3d8d4d26ae5e1b6870878ef3906f9c03013b62b
Leah X Harden <120232731+Leah-Harden@users.noreply.github.com>
2023-03-07 15:53:19 -0500

    added the rest of the basic files

---
+++ tests/Intern.test.js
@@ -0,0 +1,20 @@
+// TODO: Import the parent class
+
+
+
+class Car extends Vehicle {
+  constructor(id,color,carPassengers) {
+    super(id, 4 , "beep")
+    this.carPassengers = carPassengers
+    this.color = color
+  }
+  checkPassengers() {
+    console.log("this are the people" + this.carPassengers )
+  }
+  useHorn(){
+    console.log(this.sound)
+  }
+
+}
+
+
