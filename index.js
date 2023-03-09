e0701e4e86fed456184ef399ba846249487bd85e
Leah <leahxharden@gmail.com>
2023-03-08 22:35:39 -0500

    starting with the prompts

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 // TODO: Import the parent class
 
 
-askToPlayAgain() {
+const startTheProgram = () => {
   inquirer
     .prompt([
       {
-        type: "confirm",
-        name: "choice",
-        message: "Play Again?"
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your employee ?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
       },
       {
-        type: "confirm",
-        name: "choice",
-        message: "Play Again?"
+        type: 'input',
+        name: 'id',
+        message: 'what is there id?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
       },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is there is email?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: 'What this there role? ',
+        choices: ['Engieer', 'Intern', 'Manager'],
+        validate: (value) => { if (value) { return true } else { return `Please pick a license for this project.` } },
+      }
     ])
     .then(val => {
 
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -5,28 +5,48 @@ const Employee = require('../lib/Employee.js');
 test('this is me grab the name', () => {
   const name = `Mike`
   const Mike = new Employee(name,432,`email`)
-  expect(Mike.name).toBe(name);
+  expect(Mike.name).toBe(name); 
   
 });
-test('this is the function  getName', () => {
+
+test('this is the function getName', () => {
   const name = `Mike`
   const Mike = new Employee(name,432,`email`)
+  expect(Mike.getName()).toBe(`this is my name, `+ name);
   
-  expect(getName(Mike)).toBe(`this is my name,`+ name);
-  
+
 })
 it('this is me grab the id ', () => {
   const id = 432
   const Mike = new Employee(`Mike`,id,`email`)
   expect(Mike.id).toBe(id);
 });
+
+test('this is the function getId()', () => {
+  const id = 432
+  const Mike = new Employee('name', 432,`email`)
+  expect(Mike.getId()).toBe(`this is my id, `+ id);
+})
+
 it('this is me grab the email', () => {
   const email = `Mike@gmail.com`
   const Mike = new Employee(`Mike`,432,email)
     expect(Mike.email).toBe(email);
-  
   });
+  
+  it('this is THE getEmail()', () => {
+    const email = `Mike@gmail.com`
+    const Mike = new Employee(`Mike`,432,email)
+      expect(Mike.getEmail()).toBe(`this is my email, `+email);
+    });
+
+
   it('this is me grab the role', () => {
     const Mike = new Employee(`Mike`,432,`email`)
     expect(Mike.role).toBe(undefined);
+  });
+
+  it('this is the getRole()', () => {
+    const Mike = new Employee(`Mike`,432,`email`)
+    expect(Mike.getRole()).toBe(undefined);
   });
