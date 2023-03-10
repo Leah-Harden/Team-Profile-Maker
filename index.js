97ec5064280764da1d812a347691bfe697c7bf62
Leah <leahxharden@gmail.com>
2023-03-09 20:05:55 -0500

    reformatting the code

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
 const inquirer = require('inquirer')
 
+const basicQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your employee ?',
+    validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'what is their id?',
+    validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is their is email?',
+    validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+  },
+  {
+    type: 'list',
+    name: 'role',
+    message: 'What this their role? ',
+    choices: ['Engieer', 'Intern', 'Manager'],
+    validate: (value) => { if (value) { return true } else { return `Please pick a role for this employee.` } },
+  }
+]
+
 
 const startTheProgram = () => {
   inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your employee ?',
-        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: 'what is their id?',
-        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is their is email?',
-        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-      },
-      {
-        type: 'list',
-        name: 'role',
-        message: 'What this their role? ',
-        choices: ['Engieer', 'Intern', 'Manager'],
-        validate: (value) => { if (value) { return true } else { return `Please pick a role for this employee.` } },
-      }
-    ])
+    .prompt(basicQuestions)
     .then(val => {
       if (val = true) {
-        roleSwitch();
+        roleSwitch(basicQuestions);
       } else {
-      console.log('something broke');
+        console.log('something broke');
       }
     });
 }
 
-const roleSwitch=() => {
-  switch(startTheProgram.role) {
+const roleSwitch = (basicQuestions) => {
+  switch (basicQuestions.role) {
     case Engieer:
       engieerRole()
-    break;
+      break;
     case Intern:
       internRole()
-    break;
+      break;
     case Manager:
       managerRole()
-    break;
+      break;
   }
 }
 
-const engieerRole=() => {
+const engieerRole = () => {
   inquirer
     .prompt([
       {
@@ -71,82 +73,120 @@ const engieerRole=() => {
     ])
     .then(val => {
 
-        switch(val) {
-          case true:
-            startTheProgram()
+      switch (val) {
+        case true:
+          startTheProgram()
           break;
-          case false:
-            console.log('done')
+        case false:
+          console.log('done')
       }
     });
 }
 
 
-const internRole=() => {
+const internRole = () => {
 
   inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'school',
-      message: 'What is their school?',
-      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-    },
-    {
-      type: 'comfirm',
-      name: 'again',
-      message: 'You have finished an employee. Would you like to add another?',
-      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-    },
-  ])
-  .then(val => {
-      switch(val) {
+    .prompt([
+      {
+        type: 'input',
+        name: 'school',
+        message: 'What is their school?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+      },
+      {
+        type: 'comfirm',
+        name: 'again',
+        message: 'You have finished an employee. Would you like to add another?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+      },
+    ])
+    .then(val => {
+      switch (val) {
         case true:
           startTheProgram()
-        break;
+          break;
         case false:
           console.log('done')
-    }
-  });
+      }
+    });
 }
 
-const managerRole=() => {
+const managerRole = () => {
   inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'What is their office number?',
-      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-    },
-    {
-      type: 'comfirm',
-      name: 'again',
-      message: 'You have finished an employee. Would you like to add another?',
-      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
-    },
-  ])
-  .then(val => {
-      switch(val) {
+    .prompt([
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is their office number?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+      },
+      {
+        type: 'comfirm',
+        name: 'again',
+        message: 'You have finished an employee. Would you like to add another?',
+        validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+      },
+    ])
+    .then(val => {
+      switch (val) {
         case true:
           startTheProgram()
-        break;
+          break;
         case false:
           console.log('done')
-    }
-  });
+      }
+    });
 }
 
-function generateReadMe(questions) {
+function generate(questions) {
 
 
-const template =  ` 
+}
 
+const templateEngineer = ` 
+<div class="card myCard" style="width: 18rem;">
+<div class="card-body">
+    <h5 class="card-title">Engineer</h5>
+    <hr>
+    <h6 class="card-subtitle mb-2 ">Name</h6>
+    <p class="card-text"> ID: id</p>
+    <a class="card-link">email@lotmail.com</a>
+    <a class="card-link" href="#">GitHub</a>
+</div>
+</div>
+`
+//return templateEngineer
+
+const templateIntern = ` 
+<div class="card myCard" style="width: 18rem;">
+<div class="card-body">
+    <h5 class="card-title">Intern</h5>
+    <hr>
+    <h6 class="card-subtitle mb-2 ">Name</h6>
+    <p class="card-text"> ID: id</p>
+    <a class="card-link">email@lotmail.com</a>
+    <p>School</p>
+</div>
+</div>
+
+`
+//return templateIntern
+
+const templateManager = ` 
+<div class="card myCard" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Manager</h5>
+                    <hr>
+                    <h6 class="card-subtitle mb-2 ">Name</h6>
+                    <p class="card-text"> ID: id</p>
+                    <a class="card-link">email@lotmail.com</a>
+                    <p>officeNumber</p>
+                </div>
+            </div>
 
 
 
 
 `
-return template
-
-}
+//return templateManager
