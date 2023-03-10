5cc99714dbc6706abd1c2ced9fe2869fb9a57d20
Leah <leahxharden@gmail.com>
2023-03-09 19:10:25 -0500

    finishing the questions

--- index.js
+++ index.js
@@ -13,29 +13,28 @@ const startTheProgram = () => {
       {
         type: 'input',
         name: 'id',
-        message: 'what is there id?',
+        message: 'what is their id?',
         validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
       },
       {
         type: 'input',
         name: 'email',
-        message: 'What is there is email?',
+        message: 'What is their is email?',
         validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
       },
       {
         type: 'list',
         name: 'role',
-        message: 'What this there role? ',
+        message: 'What this their role? ',
         choices: ['Engieer', 'Intern', 'Manager'],
-        validate: (value) => { if (value) { return true } else { return `Please pick a license for this project.` } },
+        validate: (value) => { if (value) { return true } else { return `Please pick a role for this employee.` } },
       }
     ])
     .then(val => {
-
-      if (val.choice) {
+      if (val = true) {
         roleSwitch();
       } else {
-      console.log('fin');
+      console.log('something broke');
       }
     });
 }
@@ -60,24 +59,23 @@ const engieerRole=() => {
       {
         type: 'input',
         name: 'gitHub',
-        message: 'What is there there gitHub?',
+        message: 'What is their gitHub?',
         validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
       },
       {
-        type: 'input',
+        type: 'comfirm',
         name: 'again',
         message: 'You have finished an employee. Would you like to add aunthor?',
         validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
       },
     ])
     .then(val => {
-        switch(startTheProgram.role) {
-          case Engieer:
-            engieerRole()
+        switch(val) {
+          case true:
+            startTheProgram()
           break;
-        ;
-      } else {
-      console.log('fin');
+          case false:
+            console.log('done')
       }
     });
 }
@@ -85,8 +83,55 @@ const engieerRole=() => {
 
 const internRole=() => {
 
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'school',
+      message: 'What is their school?',
+      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+    },
+    {
+      type: 'comfirm',
+      name: 'again',
+      message: 'You have finished an employee. Would you like to add another?',
+      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+    },
+  ])
+  .then(val => {
+      switch(val) {
+        case true:
+          startTheProgram()
+        break;
+        case false:
+          console.log('done')
+    }
+  });
 }
 
 const managerRole=() => {
-
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: 'What is their office number?',
+      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+    },
+    {
+      type: 'comfirm',
+      name: 'again',
+      message: 'You have finished an employee. Would you like to add another?',
+      validate: (value) => { if (value) { return true } else { return `Please fill in the blank.` } },
+    },
+  ])
+  .then(val => {
+      switch(val) {
+        case true:
+          startTheProgram()
+        break;
+        case false:
+          console.log('done')
+    }
+  });
 }
